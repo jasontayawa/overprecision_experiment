3fdf1ab6766cc56a6398bd7b756f416a7ada3f68
jasontayawa <jasontayawa@gmail.com>
2021-04-07 18:12:12 -0400

    Please change local path to ....\_static

--- block1/models.py
+++ block1/models.py
@@ -27,6 +27,7 @@ class Constants(BaseConstants):
 
 class Subsession(BaseSubsession):
 
+    #table 1 variables
     Rrrr = models.IntegerField()
     Rrrb = models.IntegerField()
     Rrbr = models.IntegerField()
@@ -45,7 +46,53 @@ class Subsession(BaseSubsession):
     Bbbr = models.IntegerField()
     Bbbb = models.IntegerField()
 
+    #table 2 variables given s1,s2 = rr
 
+    Rrr_rr = models.IntegerField()
+    Rrb_rr = models.IntegerField()
+    Rbr_rr = models.IntegerField()
+    Rbb_rr = models.IntegerField()
+
+    Brr_rr = models.IntegerField()
+    Brb_rr = models.IntegerField()
+    Bbr_rr = models.IntegerField()
+    Bbb_rr = models.IntegerField()
+
+    # table 2 variables given s1,s2 = rb
+
+    Rrr_rb = models.IntegerField()
+    Rrb_rb = models.IntegerField()
+    Rbr_rb = models.IntegerField()
+    Rbb_rb = models.IntegerField()
+
+    Brr_rb = models.IntegerField()
+    Brb_rb = models.IntegerField()
+    Bbr_rb = models.IntegerField()
+    Bbb_rb = models.IntegerField()
+
+    # table 2 variables given s1,s2 = br
+
+    Rrr_br = models.IntegerField()
+    Rrb_br = models.IntegerField()
+    Rbr_br = models.IntegerField()
+    Rbb_br = models.IntegerField()
+
+    Brr_br = models.IntegerField()
+    Brb_br = models.IntegerField()
+    Bbr_br = models.IntegerField()
+    Bbb_br = models.IntegerField()
+
+    # table 2 variables given s1,s2 = bb
+
+    Rrr_bb = models.IntegerField()
+    Rrb_bb = models.IntegerField()
+    Rbr_bb = models.IntegerField()
+    Rbb_bb = models.IntegerField()
+
+    Brr_bb = models.IntegerField()
+    Brb_bb = models.IntegerField()
+    Bbr_bb = models.IntegerField()
+    Bbb_bb = models.IntegerField()
 
     def creating_session(self):
         df = pd.read_csv('block1/Block1rounds.csv')
@@ -61,6 +108,8 @@ class Subsession(BaseSubsession):
             p.signal2 = df['Signal2'][(20 * (p.partition - 1)) + self.round_number - 1]
             p.signal3 = df['Signal3'][(20 * (p.partition - 1)) + self.round_number - 1]
             p.signal4 = df['Signal4'][(20 * (p.partition - 1)) + self.round_number - 1]
+
+        #Table 1 showing Signals 1,2, and 3
 
         self.Rrrr = df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 1)].count()['Color']
 
@@ -94,6 +143,125 @@ class Subsession(BaseSubsession):
 
         self.Bbbb = df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 0)].count()['Color']
 
+        # Table 2 showing Signals 3 & 4 given s1,s2 = rr
+
+        self.Rrr_rr = df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()['Color']
+
+        self.Rrb_rr = df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()['Color']
+
+        self.Rbr_rr = df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()['Color']
+
+        self.Rbb_rr = df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()['Color']
+
+        self.Brr_rr = df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()['Color']
+
+        self.Brb_rr = df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()['Color']
+
+        self.Bbr_rr = df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()['Color']
+
+        self.Bbb_rr = df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()['Color']
+
+        # Table 2 showing Signals 3 & 4 given s1,s2 = rb
+
+        self.Rrr_rb = \
+        df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rrb_rb = \
+        df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Rbr_rb = \
+        df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rbb_rb = \
+        df[(df.Color == 1) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Brr_rb = \
+        df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Brb_rb = \
+        df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Bbr_rb = \
+        df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Bbb_rb = \
+        df[(df.Color == 0) & (df.Signal1 == 1) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        # Table 2 showing Signals 3 & 4 given s1,s2 = br
+
+        self.Rrr_br = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rrb_br = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Rbr_br = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rbb_br = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Brr_br = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Brb_br = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Bbr_br = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Bbb_br = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 1) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        # Table 2 showing Signals 3 & 4 given s1,s2 = bb
+
+        self.Rrr_bb = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rrb_bb = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Rbr_bb = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Rbb_bb = \
+        df[(df.Color == 1) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Brr_bb = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Brb_bb = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 1) & (df.Signal4 == 0)].count()[
+            'Color']
+
+        self.Bbr_bb = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 1)].count()[
+            'Color']
+
+        self.Bbb_bb = \
+        df[(df.Color == 0) & (df.Signal1 == 0) & (df.Signal2 == 0) & (df.Signal3 == 0) & (df.Signal4 == 0)].count()[
+            'Color']
 
 class Group(BaseGroup):
     def profit_calculation(self):
@@ -147,12 +315,12 @@ class Player(BasePlayer):
     guess1 = models.IntegerField(initial=50,
                                min=0,
                                max=100,
-                               label="(Guess 1) What do you this is the chance that the assign color for the round is Red",
+                               label="Guess 1: What do you this is the chance that the assign color for the round is Red?",
                                widget=widgets.Slider)
     guess2 = models.IntegerField(initial=50,
                                min=0,
                                max=100,
-                               label="(Guess 2) What do you this is the chance that the assign color for the round is Red",
+                               label="Guess 2: What do you this is the chance that the assign color for the round is Red?",
                                widget=widgets.Slider)
 
     color = models.IntegerField()
--- block1/pages.py
+++ block1/pages.py
@@ -82,6 +82,55 @@ class Guess2(Page):
         Bbrb = self.subsession.Bbrb
         Bbbr = self.subsession.Bbbr
         Bbbb = self.subsession.Bbbb
+
+        # table 2 variables given s1,s2 = rr
+
+        Rrr_rr = self.subsession.Rrr_rr
+        Rrb_rr = self.subsession.Rrb_rr
+        Rbr_rr = self.subsession.Rbr_rr
+        Rbb_rr = self.subsession.Rbb_rr
+
+        Brr_rr = self.subsession.Brr_rr
+        Brb_rr = self.subsession.Brb_rr
+        Bbr_rr = self.subsession.Bbr_rr
+        Bbb_rr = self.subsession.Bbb_rr
+
+        # table 2 variables given s1,s2 = rb
+
+        Rrr_rb = self.subsession.Rrr_rb
+        Rrb_rb = self.subsession.Rrb_rb
+        Rbr_rb = self.subsession.Rbr_rb
+        Rbb_rb = self.subsession.Rbb_rb
+
+        Brr_rb = self.subsession.Brr_rb
+        Brb_rb = self.subsession.Brb_rb
+        Bbr_rb = self.subsession.Bbr_rb
+        Bbb_rb = self.subsession.Bbb_rb
+
+        # table 2 variables given s1,s2 = br
+
+        Rrr_br = self.subsession.Rrr_br
+        Rrb_br = self.subsession.Rrb_br
+        Rbr_br = self.subsession.Rbr_br
+        Rbb_br = self.subsession.Rbb_br
+
+        Brr_br = self.subsession.Brr_br
+        Brb_br = self.subsession.Brb_br
+        Bbr_br = self.subsession.Bbr_br
+        Bbb_br = self.subsession.Bbb_br
+
+        # table 2 variables given s1,s2 = bb
+
+        Rrr_bb = self.subsession.Rrr_bb
+        Rrb_bb = self.subsession.Rrb_bb
+        Rbr_bb = self.subsession.Rbr_bb
+        Rbb_bb = self.subsession.Rbb_bb
+
+        Brr_bb = self.subsession.Brr_bb
+        Brb_bb = self.subsession.Brb_bb
+        Bbr_bb = self.subsession.Bbr_bb
+        Bbb_bb = self.subsession.Bbb_bb
+
         return dict(
             s1=s1,
             s2=s2,
@@ -103,6 +152,38 @@ class Guess2(Page):
             Bbrb=Bbrb,
             Bbbr=Bbbr,
             Bbbb=Bbbb,
+            Rrr_rr=Rrr_rr,
+            Rrb_rr=Rrb_rr,
+            Rbr_rr=Rbr_rr,
+            Rbb_rr=Rbb_rr,
+            Brr_rr=Brr_rr,
+            Brb_rr=Brb_rr,
+            Bbr_rr=Bbr_rr,
+            Bbb_rr=Bbb_rr,
+            Rrr_rb=Rrr_rb,
+            Rrb_rb=Rrb_rb,
+            Rbr_rb=Rbr_rb,
+            Rbb_rb=Rbb_rb,
+            Brr_rb=Brr_rb,
+            Brb_rb=Brb_rb,
+            Bbr_rb=Bbr_rb,
+            Bbb_rb=Bbb_rb,
+            Rrr_br=Rrr_br,
+            Rrb_br=Rrb_br,
+            Rbr_br=Rbr_br,
+            Rbb_br=Rbb_br,
+            Brr_br=Brr_br,
+            Brb_br=Brb_br,
+            Bbr_br=Bbr_br,
+            Bbb_br=Bbb_br,
+            Rrr_bb=Rrr_bb,
+            Rrb_bb=Rrb_bb,
+            Rbr_bb=Rbr_bb,
+            Rbb_bb=Rbb_bb,
+            Brr_bb=Brr_bb,
+            Brb_bb=Brb_bb,
+            Bbr_bb=Bbr_bb,
+            Bbb_bb=Bbb_bb,
         )
 
 class ResultsWaitPage(WaitPage):
