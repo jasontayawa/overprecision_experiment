d7ced0cfd74999404ae4fb75955513236740711b
jasontayawa <jasontayawa@gmail.com>
2021-04-24 17:35:20 -0400

    Please change local path to ....\_static

--- block1/models.py
+++ block1/models.py
@@ -10,6 +10,14 @@ from otree.api import (
 )
 import random
 import pandas as pd
+import numpy
+from psycopg2.extensions import register_adapter, AsIs
+def addapt_numpy_float64(numpy_float64):
+    return AsIs(numpy_float64)
+def addapt_numpy_int64(numpy_int64):
+    return AsIs(numpy_int64)
+register_adapter(numpy.float64, addapt_numpy_float64)
+register_adapter(numpy.int64, addapt_numpy_int64)
 
 
 author = 'Your name here'
